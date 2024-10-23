130090601165cef5e12157b2333ba02df1fa5338
Rakesh-Biswal <rb2306114@gmail.com>
2024-10-23 18:04:23 +0530

    hello final

--- server.js
+++ server.js
@@ -26,22 +26,22 @@ try {
 
     admin.initializeApp({
         credential: admin.credential.cert({
-            "type": process.env.FIREBASE_TYPE,
-            "project_id": process.env.FIREBASE_PROJECT_ID,
-            "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-            "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Properly handle the newlines
-            "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-            "client_id": process.env.FIREBASE_CLIENT_ID,
-            "auth_uri": process.env.FIREBASE_AUTH_URI,
-            "token_uri": process.env.FIREBASE_TOKEN_URI,
-            "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_CERT,
-            "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL
+            type: process.env.FIREBASE_TYPE,
+            project_id: process.env.FIREBASE_PROJECT_ID,
+            private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+            private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Ensure proper formatting of the private key
+            client_email: process.env.FIREBASE_CLIENT_EMAIL,
+            client_id: process.env.FIREBASE_CLIENT_ID,
+            auth_uri: process.env.FIREBASE_AUTH_URI,
+            token_uri: process.env.FIREBASE_TOKEN_URI,
+            auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+            client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
         }),
         storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     });
 
     bucket = admin.storage().bucket();
-    
+
     console.log('✅ Firebase Initialized');
     
 } catch (err) {
