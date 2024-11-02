bf62393083cec7f87990c613daf791766c695d26
Rakesh-Biswal <rb2306114@gmail.com>
2024-11-02 21:04:38 +0530

    ef

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ try {
     const admin = require('firebase-admin');
 
     admin.initializeApp({
+        
         credential: admin.credential.cert({
             type: process.env.FIREBASE_TYPE,
             project_id: process.env.FIREBASE_PROJECT_ID,
