977b2878933742a1015c8a7bd351c8b62bd20900
Rakesh-Biswal <rb2306114@gmail.com>
2024-11-03 10:11:17 +0530

    fcdg

--- models/Worker.js
+++ models/Worker.js
@@ -10,7 +10,7 @@ const workerSchema = new mongoose.Schema({
     profession: { type: String, required: true },
     experience: { type: Number, required: true },
     location: { type: String, required: true },
-    status: { type: String, enum: ['Active', 'Busy'], default: 'Active' } // New status field
+    status: { type: String, default: "Active" }, // Add this field
 }, { timestamps: true });
 
 module.exports = mongoose.model('Worker', workerSchema);
